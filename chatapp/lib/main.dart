7956ae3a93c8fd7dd8a036ed8e322086e73d78d4
mahendra <mahendra.ahada@beretail.in>
2021-02-04 10:37:40 +0530

    Basic app set up, created welcome screen

---
+++ chatapp/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import './screens/welcome.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const MaterialColor PrimaryColor = const MaterialColor(
+    0xFF363636,
+    const <int, Color>{
+      50: const Color(0xFF363636),
+      100: const Color(0xFF363636),
+      200: const Color(0xFF363636),
+      300: const Color(0xFF363636),
+      400: const Color(0xFF363636),
+      500: const Color(0xFF363636),
+      600: const Color(0xFF363636),
+      700: const Color(0xFF363636),
+      800: const Color(0xFF363636),
+      900: const Color(0xFF363636),
+    },
+  );
+
+  static const MaterialColor PrimaryBackGround = const MaterialColor(
+    0xFFDF193E,
+    const <int, Color>{
+      50: const Color(0xFFDF193E),
+      100: const Color(0xFFDF193E),
+      200: const Color(0xFFDF193E),
+      300: const Color(0xFFDF193E),
+      400: const Color(0xFFDF193E),
+      500: const Color(0xFFDF193E),
+      600: const Color(0xFFDF193E),
+      700: const Color(0xFFDF193E),
+      800: const Color(0xFFDF193E),
+      900: const Color(0xFFDF193E),
+    },
+  );
+
+  static const MaterialColor PrimaryLight = const MaterialColor(
+    0xFFB9B9B9,
+    const <int, Color>{
+      50: const Color(0xFFB9B9B9),
+      100: const Color(0xFFB9B9B9),
+      200: const Color(0xFFB9B9B9),
+      300: const Color(0xFFB9B9B9),
+      400: const Color(0xFFB9B9B9),
+      500: const Color(0xFFB9B9B9),
+      600: const Color(0xFFB9B9B9),
+      700: const Color(0xFFB9B9B9),
+      800: const Color(0xFFB9B9B9),
+      900: const Color(0xFFB9B9B9),
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: PrimaryColor,
+          backgroundColor: PrimaryBackGround,
+          accentColor: PrimaryLight,
+          fontFamily: 'lato'),
+      home: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Welcome(),
+      ),
+    );
+  }
+}
---
+++ chatapp/lib/screens/welcome.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.1, 0.9],
+          colors: [const Color(0xffdf193e), const Color(0xffeb402c)],
+        ),
+      ),
+      child: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'HeyU',
+                style: new TextStyle(
+                  fontSize: 70,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Lets connect with your besties!!!',
+                style: new TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                autocorrect: true,
+                textCapitalization: TextCapitalization.words,
+                enableSuggestions: false,
+                style: TextStyle(color: Colors.white),
+                initialValue: 'supertramp@supertramp.com',
+                textAlign: TextAlign.center,
+                decoration: new InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                      borderSide: BorderSide.none),
+                  contentPadding: new EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 10.0),
+                  fillColor: const Color(0xff9e1a27),
+                  filled: true,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                style: TextStyle(color: Colors.white),
+                initialValue: '123456789',
+                textAlign: TextAlign.center,
+                decoration: new InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                      borderSide: BorderSide.none),
+                  contentPadding: new EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 10.0),
+                  fillColor: const Color(0xff9e1a27),
+                  filled: true,
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: FlatButton(
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  color: Colors.white,
+                  child: Text(
+                    'SignUp',
+                    style: new TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xffdf193e),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              FlatButton(
+                child: Text(
+                  'Already have an account? Sign in',
+                  style: new TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
